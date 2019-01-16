a92d55c0b6eb4bdf69361dcf107dc4b8549a8866
scdev <28826596+scdev0@users.noreply.github.com>
2019-01-16 00:56:59 -0500

    Create to-do item service and inject into to-do controller

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
@@ -6,14 +9,28 @@ namespace AspNetCoreTodo.Controllers
     /* The controller that handles actions routed to /Todo/* */
     public class TodoController : Controller
     {
+        // private variable to reference the ITodoItemService
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
         // Actions go here
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             // Get to-do items from db
+            var items = await _todoItemService.GetIncompleteItemsAsync();
 
-            // Put items into a model
+            // Put items into the view model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
 
             // Render view using the model
+            return View(model);
         }
     }
 }
--- AspNetCoreTodo/Models/TodoViewModel.cs
+++ AspNetCoreTodo/Models/TodoViewModel.cs
@@ -1,5 +1,6 @@
 namespace AspNetCoreTodo.Models
 {
+    /* The view model holding to-do items to be displayed */
     public class TodoViewModel
     {
         public TodoItem[] Items { get; set; }
